626f2e28add4c01d0ed1bf21def8dd900aadca89
JadeWeaver <jadeweaver.ann@gmail.com>
2024-02-08 21:01:48 -0500

    changed rapid test to Buckleys

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -4,14 +4,15 @@ class RapidTestOrder {
       WELCOMING: () => {
         let aReturn = [];
         this.stateCur = this.OrderState.RESERVING;
-        aReturn.push("Welcome to Rich's Rapid Test.");
-        aReturn.push("Would you like to reserve a rapid test kit?");
+        aReturn.push("Welcome to Buckley's Coffee House.");
+        aReturn.push("What size drink would you like to order[small, medium, large]?");
         return aReturn;
       },
+      
       RESERVING: (sInput) => {
         let aReturn = [];
         this.isDone = true;
-        if (sInput.toLowerCase().startsWith('y')) {
+        if (sInput.toLowerCase().startsWith('s')) {
           aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
@@ -22,7 +23,8 @@ class RapidTestOrder {
         }
         return aReturn;
       }
-    };
+    
+
 
     this.stateCur = this.OrderState.WELCOMING;
     this.isDone = false;
