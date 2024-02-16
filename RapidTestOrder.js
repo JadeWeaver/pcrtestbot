bbb4f3aab61551235355cc4c6ac0f6b20c6ad351
JadeWeaver <jadeweaver.ann@gmail.com>
2024-02-15 20:12:20 -0500

    added topping

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -5,7 +5,7 @@ class RapidTestOrder {
         let aReturn = [];
         this.stateCur = this.OrderState.SIZE;
         aReturn.push("Welcome to Buckley's Coffee House.");
-        aReturn.push("What size drink would you like to order: small[+$3.00], medium[+$4.00], or large?[+$5.00]");
+        aReturn.push("What size drink would you like to order: small [+$3.00], medium [+$4.00], or large? [+$5.00]");
         return aReturn;
       },
       SIZE: (sInput) =>{
@@ -40,12 +40,12 @@ class RapidTestOrder {
           this.Drink = "Macchiato";
         } 
         aReturn.push("What type of milk would you like?");
-        aReturn.push("We have Whole[+$1.00] and Almond[+$2.00]");
+        aReturn.push("We have Whole [+$1.00] and Almond [+$2.00]");
         return aReturn;
       },
       MILK: (sInput) =>{
         let aReturn = [];
-        this.stateCur = this.OrderState.FINALIZE;
+        this.stateCur = this.OrderState.TOPPING;
         if (sInput.toLowerCase().startsWith('a')){
           this.Milk = "almond";
           this.Cost = this.Cost + 2;
@@ -54,7 +54,22 @@ class RapidTestOrder {
             this.Milk = "whole";
             this.Cost = this.Cost + 1;
         }
-        aReturn.push("Would you like to add a bagel to your order for an extra $3.00?");
+        aReturn.push("What topping would you like?");
+        aReturn.push("We have Whipped Cream [+$1.00], Caramel [+$1.00], or No Topping?");
+        return aReturn;
+      },
+      TOPPING: (sInput) =>{
+        let aReturn = [];
+        this.stateCur = this.OrderState.FINALIZE;
+        if (sInput.toLowerCase().startsWith('w')){
+          this.Top = "whipped cream";
+          this.Cost = this.Cost + 1;
+        } 
+        else if(sInput.toLowerCase().startsWith('c')){
+            this.Top = "caramel";
+          this.Cost = this.Cost + 1;
+        }
+        aReturn.push("Thank you for choosing Buckley's Coffee House. Before you finalize your order, would you like to add a bagel for an extra $3.00?");
         return aReturn;
       },
       FINALIZE: (sInput) =>{
@@ -71,13 +86,13 @@ class RapidTestOrder {
         let aReturn = [];
         this.isDone = true;
         if (sInput.toLowerCase().startsWith('y') && this.Bagel){
-          aReturn.push(`Your online order of a ${this.Size} ${this.Drink} coffee with ${this.Milk} milk and a bagel is reserved under the phone number ${this.sFrom}. Your total is ${this.Cost}`);
+          aReturn.push(`Your online order of a ${this.Size} ${this.Drink} coffee with ${this.Milk} milk, ${this.Top}, and a bagel is reserved under the phone number ${this.sFrom}. Your total is $${this.Cost}.00`);
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
           aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
         } 
         else if (sInput.toLowerCase().startsWith('y')){
-          aReturn.push(`Your online order of a ${this.Size} ${this.Drink} coffee with ${this.Milk} milk is reserved under the phone number ${this.sFrom}.Your total is ${this.Cost}`);
+          aReturn.push(`Your online order of a ${this.Size} ${this.Drink} coffee with ${this.Milk} milk and ${this.Top} is reserved under the phone number ${this.sFrom}.Your total is $${this.Cost}.00`);
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
           aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
@@ -100,6 +115,7 @@ class RapidTestOrder {
     this.Size = "small";
     this.Drink = "regular";
     this.Cost = 0;
+    this.Top = "no toppings";
   }
   handleInput(sInput) {
     return this.stateCur(sInput);
